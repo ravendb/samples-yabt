004f33c431ccb989ff4723fd44ed4e7df8855b22
Alex Klaus <alex.a.klaus@gmail.com>
2020-11-26 20:42:35 +1000

    Fixed converting DTO to Dictionary<string,string> for later use in query strings

--- tools/TicketImporter/Helpers/DtoConversion.cs
+++ tools/TicketImporter/Helpers/DtoConversion.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 using Microsoft.Extensions.Primitives;
@@ -16,13 +17,21 @@ namespace Raven.Yabt.TicketImporter.Helpers
 		/// </summary>
 		public static Dictionary<string, StringValues> ToDictionary<T>(T dto)
 		{
+			// Step 1. Convert the DTO to a JSON
 			var jsonSettings = new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore };
 			jsonSettings.Converters.Add(new StringEnumConverter());
 			var json = JsonConvert.SerializeObject(dto, jsonSettings);
+			
+			// Step 2. Convert the JSON to JObject
+			var settings = new JsonSerializerSettings { DateParseHandling = DateParseHandling.None };
+			var jsonObject = JsonConvert.DeserializeObject<JObject>(json, settings);
+			if (jsonObject == null)
+				throw new ApplicationException("Can't deserialize object: "+json);
 
-			var jsonObject = JObject.Parse(json);
+			// Step 3. Get a collection of JToken
 			var jTokens = jsonObject.Descendants().Where(p => !p.Any());
 
+			// Step 4. Convert to a dictionary
 			var results = jTokens.Aggregate(
 				new Dictionary<string, StringValues>(),
 				(pairs, jToken) =>
