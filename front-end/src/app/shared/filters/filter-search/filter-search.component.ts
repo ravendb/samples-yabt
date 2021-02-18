75f374a59f16efaa392292c8889ef4810296d95b
Alex Klaus <alex.a.klaus@gmail.com>
2021-02-18 22:20:04 +1000

    Added a base custom control and derived 'filter-search' from it

--- front-end/src/app/shared/filters/filter-search/filter-search.component.ts
+++ front-end/src/app/shared/filters/filter-search/filter-search.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
-import { ControlContainer, ControlValueAccessor, FormControl, FormControlDirective, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ControlContainer, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { CustomFormControlBase } from '@shared/forms/custom-form-control-base';
 import { Subscription } from 'rxjs';
 import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
@@ -15,53 +16,19 @@ import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 		},
 	],
 })
-export class FilterSearchComponent implements ControlValueAccessor, OnInit, OnDestroy {
-	@ViewChild(FormControlDirective, { static: true })
-	formControlDirective!: FormControlDirective;
-	@Input()
-	formControl: FormControl | undefined;
-	@Input()
-	formControlName: string | undefined;
-
-	/* 
-		Resolve FormControl instance no matter `formControl` or `formControlName` is given. 
-		If formControlName is given, then this.controlContainer.control is the parent FormGroup (or FormArray) instance. 
-	*/
-	get control(): AbstractControl | undefined {
-		return this.formControl || (!!this.formControlName && this.controlContainer.control?.get(this.formControlName)) || undefined;
-	}
-
-	searchControl: FormControlTyped<string> = new FormControl('');
-
-	/* View -> model callback called when select has been touched */
-	public onTouched: () => void = () => {};
-	/* View -> model callback called when value changes from the UI */
-	private _onChange: (value?: string) => void = () => {};
-
+export class FilterSearchComponent extends CustomFormControlBase<string> implements OnInit, OnDestroy {
 	private _subscriptions: Subscription = new Subscription();
 
-	constructor(private controlContainer: ControlContainer) {}
+	constructor(controlContainer: ControlContainer) {
+		super(controlContainer);
+	}
 
 	ngOnInit() {
 		this._subscriptions.add(
-			this.searchControl.valueChanges.pipe(distinctUntilChanged(), debounceTime(300)).subscribe((val: string) => this._onChange(val))
+			this.mainControl.valueChanges.pipe(distinctUntilChanged(), debounceTime(300)).subscribe((val: string) => this._onChange(val))
 		);
 	}
 	ngOnDestroy() {
 		this._subscriptions.unsubscribe();
-	}
-
-	registerOnChange(fn: (value?: string) => void): void {
-		this._onChange = fn;
-	}
-	registerOnTouched(fn: () => {}): void {
-		this.onTouched = fn;
-	}
-	writeValue(val: string): void {
-		this.searchControl.setValue(val, { emitEvent: false });
-	}
-	setDisabledState(isDisabled: boolean): void {
-		if (isDisabled) this.searchControl.disable();
-		else this.searchControl.enable();
 	}
 }
