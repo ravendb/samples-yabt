a8401a43ac44a5ba06aab8de99f1c2beb5e4c620
Alex Klaus <alex.a.klaus@gmail.com>
2021-03-08 10:10:14 +1000

    Added showing the name of the current user in the menu

--- front-end/src/app/core/main-menu/main-menu.component.ts
+++ front-end/src/app/core/main-menu/main-menu.component.ts
@@ -2,6 +2,8 @@ import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { AfterViewInit, ChangeDetectorRef, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
 import { Router } from '@angular/router';
+import { CurrentUserResponse } from '@core/api-models/user/item/CurrentUserResponse';
+import { UsersService } from '@core/api-services/users.service';
 import { get, some } from 'lodash-es';
 import { Subscription } from 'rxjs';
 import { MainMenuMapItem } from './main-menu-map-item';
@@ -20,13 +22,23 @@ export class MainMenuComponent implements OnInit, AfterViewInit, OnDestroy {
 	@ViewChild(MatSidenav)
 	private sidenav!: MatSidenav;
 
-	public get siteMap() {
-		return this._siteMap;
+	// Items of the side menu
+	readonly siteMap: MainMenuMapItem[] = [
+		new MainMenuMapItem('Backlog Items', '/backlog-items', ['/backlog-items'], 'bug_report'),
+		new MainMenuMapItem('Users', '/users', ['/users'], 'supervisor_account'),
+	];
+
+	get currentUser(): CurrentUserResponse | undefined {
+		return this._currentUser;
 	}
-	public get isMobile() {
+	get currentUserLink(): string | undefined {
+		return !!this.currentUser?.id ? `/users/${this.currentUser.id}` : '';
+	}
+	get isMobile() {
 		return this._isMobile;
 	}
-	public get isNarrowMenu() {
+
+	get isNarrowMenu() {
 		return !this._isMobile && this._isNarrowMenu;
 	}
 	get mobileTitle(): string {
@@ -37,27 +49,25 @@ export class MainMenuComponent implements OnInit, AfterViewInit, OnDestroy {
 		);
 	}
 
-	// TODO: Request user;s details from the back-end
-	userId = 0;
-	userName = 'Test User';
-
-	// Items of the side menu
-	private _siteMap: MainMenuMapItem[] = [
-		new MainMenuMapItem('Backlog Items', '/backlog-items', ['/backlog-items'], 'bug_report'),
-		new MainMenuMapItem('Users', '/users', ['/users'], 'supervisor_account'),
-	];
-
 	private _isMobile: boolean = false;
 	// Flag: whether the side menu is collapsed
 	private _isNarrowMenu: boolean = true;
-	private subscriptions: Subscription = new Subscription();
+	private _currentUser: CurrentUserResponse | undefined;
+	private _subscriptions: Subscription = new Subscription();
 
-	constructor(private breakpoint: BreakpointObserver, private router: Router, private cdr: ChangeDetectorRef) {}
+	constructor(
+		private breakpoint: BreakpointObserver,
+		private router: Router,
+		private cdr: ChangeDetectorRef,
+		private userService: UsersService
+	) {}
 
-	ngOnInit(): void {}
+	ngOnInit(): void {
+		this._subscriptions.add(this.userService.getCurrentUser().subscribe(r => (this._currentUser = r)));
+	}
 
 	ngAfterViewInit(): void {
-		this.subscriptions.add(
+		this._subscriptions.add(
 			// It's very frustrating that this properties can't be controlled from CSS
 			this.breakpoint.observe([Breakpoints.Handset]).subscribe(result => {
 				this._isMobile = result.matches;
@@ -75,7 +85,7 @@ export class MainMenuComponent implements OnInit, AfterViewInit, OnDestroy {
 	}
 
 	ngOnDestroy(): void {
-		this.subscriptions.unsubscribe();
+		this._subscriptions.unsubscribe();
 	}
 
 	// Collapse/expand the menu
