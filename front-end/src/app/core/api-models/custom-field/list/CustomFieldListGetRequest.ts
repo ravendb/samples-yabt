4bbe44f03fe1f27138bb48f2e934e7b91050dddf
Alex Klaus <alex.a.klaus@gmail.com>
2021-04-03 16:40:08 +1000

    Custom Fields: Added filtering by backlog item type

--- front-end/src/app/core/api-models/custom-field/list/CustomFieldListGetRequest.ts
+++ front-end/src/app/core/api-models/custom-field/list/CustomFieldListGetRequest.ts
@@ -1,5 +1,7 @@
+import { BacklogItemType } from '@core/api-models/common/backlog-item';
 import { ListRequest } from '@core/api-models/common/ListRequest';
 
 export class CustomFieldListGetRequest extends ListRequest {
 	ids: string[] | undefined;
+	backlogItemType: keyof typeof BacklogItemType | undefined;
 }
