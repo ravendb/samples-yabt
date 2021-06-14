f9fab5f7a02b51f47aa67113eca170526e2fe099
Alex Klaus <alex.a.klaus@gmail.com>
2021-06-14 10:43:44 +1000

    Experiments on intercepting DB server operations to inject/validate the current Tenant ID

---
+++ back-end/Database/SetupDocumentStore.MultitenancyExtension.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Linq;
+
+using Raven.Client.Documents;
+using Raven.Client.Documents.Session;
+using Raven.Yabt.Database.Common.Helpers;
+using Raven.Yabt.Database.Models;
+
+using Sparrow.Json;
+
+namespace Raven.Yabt.Database
+{
+	public static partial class SetupDocumentStore
+	{
+		private static partial void SetupMultitenancy(this IDocumentStore store, Func<string> tenantResolverFunc)
+		{
+			store.OnBeforeQuery += (_, args) => AddFilteringByTenantIdToSelectQueries(args.QueryCustomization, tenantResolverFunc());
+			store.OnAfterConversionToEntity += (_, args) => ValidateRightTenantOnLoadingEntity(args.Document, tenantResolverFunc());
+			store.OnBeforeConversionToEntity += (sender, args) => { };
+			store.OnBeforeDelete += (sender, args) => { };
+			store.OnBeforeStore += (sender, args) => { };
+		}
+
+		private static void AddFilteringByTenantIdToSelectQueries(IDocumentQueryCustomization customization, string currentTenantId)
+		{
+			// The lines below could have been simplified to `entityType = args.QueryCustomization.GetType().GetGenericArguments()[0]` 
+
+			var type = customization.GetType();
+			var entityType = type.GetInterfaces()
+			                     .SingleOrDefault(i => 
+				                     i.IsClosedTypeOf(typeof(IDocumentQuery<>)) || 
+				                     i.IsClosedTypeOf(typeof(IAsyncDocumentQuery<>)))
+			                     ?.GetGenericArguments()
+			                     .SingleOrDefault();
+			if (entityType?.IsAssignableTo<ITenantedEntity>() != true) 
+				return;
+				
+			// Add the "AND" to the the WHERE clause 
+			// (the method has a check under the hood to prevent adding "AND" if the "WHERE" is empty)
+			type.GetDeclaredMethod(
+				    nameof(IDocumentQuery<object>.AndAlso))
+			    .Invoke(customization, null);
+			// Add "TenantId = 'Bla'" into the WHERE clause
+			type.GetDeclaredMethod(
+				    nameof(IDocumentQuery<object>.WhereEquals),
+				    new[] { typeof(string), typeof(object), typeof(bool) })
+			    .Invoke(
+				    customization,
+				    new object[]
+				    {
+					    nameof(ITenantedEntity.TenantId),
+					    currentTenantId,
+					    true,
+				    });
+		}
+
+		private static void ValidateRightTenantOnLoadingEntity(BlittableJsonReaderObject document, string currentTenantId)
+		{
+			if ((string) document[nameof(ITenantedEntity.TenantId)] != currentTenantId)
+				throw new Exception("AAA");
+		}
+	}
+}
--- back-end/Database/SetupDocumentStore.cs
+++ back-end/Database/SetupDocumentStore.cs
@@ -1,22 +1,18 @@
 ﻿using System;
-using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 
 using Newtonsoft.Json;
 
 using Raven.Client.Documents;
 using Raven.Client.Documents.Conventions;
-using Raven.Client.Documents.Session;
 using Raven.Client.Json.Serialization.NewtonsoftJson;
 
 using Raven.Yabt.Database.Common.Configuration;
-using Raven.Yabt.Database.Common.Helpers;
-using Raven.Yabt.Database.Models;
 using Raven.Yabt.Database.Models.BacklogItems;
 
 namespace Raven.Yabt.Database
 {
-	public static class SetupDocumentStore
+	public static partial class SetupDocumentStore
 	{
 		/// <summary>
 		///     Configure RavenDB Document Store
@@ -25,7 +21,7 @@ namespace Raven.Yabt.Database
 		{
 			store.Conventions.UseOptimisticConcurrency = true;
 
-			// Added this so that when the property is missing in the DB, default values are assigned during serialization
+			// If a property is missing in the DB, default values are assigned during serialization
 			store.Conventions.Serialization = new NewtonsoftJsonSerializationConventions
 				{
 					CustomizeJsonSerializer = serializer => serializer.NullValueHandling = NullValueHandling.Ignore
@@ -39,41 +35,10 @@ namespace Raven.Yabt.Database
 
 					return DocumentConventions.DefaultGetCollectionName(type);
 				};
-			//if (tenantResolverFunc != null)
-			{
-				store.OnBeforeQuery += (_, args) =>
-				{
-					//Type argument = args.QueryCustomization.GetType().GetGenericArguments()[0];
-					//argument.IsAssignableFrom(typeof(ITenantData));
-					var customization = args.QueryCustomization;
-					//q.WhereEquals("TenantId", "1234");
-
-					var type = customization.GetType();
-					var entityType = type.GetInterfaces()
-					                     .SingleOrDefault(
-						                     i => i.IsClosedTypeOf(typeof(IDocumentQuery<>)) ||
-							                     i.IsClosedTypeOf(typeof(IAsyncDocumentQuery<>)))
-					                     ?.GetGenericArguments()
-					                     .Single();
-					if (entityType != null && entityType.IsAssignableTo<ITenantedEntity>())
-					{
-						(customization as IAsyncDocumentQuery<>).AndAlso()
-						
-						// Add the "AND" to the the WHERE clause 
-						// (the method has a check under the hood to prevent adding "AND" if the "WHERE" is empty)
-						type.GetDeclaredMethod("AndAlso").Invoke(customization, null);
-						// Add "TenantId = 'Bla'" into the WHERE clause
-						type.GetDeclaredMethod("WhereEquals", new[] { typeof(string), typeof(object) })
-						    .Invoke(
-							    customization,
-							    new object[]
-							    {
-								    nameof(ITenantedEntity.TenantId),
-								    "tenantResolverFunc()"
-							    });
-					}
-				};
-			}
+			
+			if (tenantResolverFunc != null)
+				// Setup multi-tenancy
+				store.SetupMultitenancy(tenantResolverFunc);
 		}
 
 		/// <summary>
@@ -109,5 +74,7 @@ namespace Raven.Yabt.Database
 			
 			return store;
 		}
+		
+		private static partial void SetupMultitenancy(this IDocumentStore store, Func<string> tenantResolverFunc);
 	}
 }
